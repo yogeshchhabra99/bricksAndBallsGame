5958b10ec4ff845f7469d7add1096f8ccb9e7831
Yogesh Chhabra <chhabra99yogesh@gmail.com>
2019-06-11 23:20:48 +0530

    game complete

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
 This is the old game where you have to break bricks using a ball. The ball can fell only on a platform controlled by the player. This is a simple version with constant speeds of the ball and the platform.
+Rules and instructions:
+1.) You can control platform right/left using arrow keys, ball must fall on the platform or you lose.
+2.) As the time passes score decreases.
+3.) Hitting a brick increases score.
+4.) You havee to hit all bricks asap.
+5.) Each point on the platform adds a specific component of x velocity to the ball.
+6.) Use P to pause
 Goal is to create train an AI player for this game.
-Current Status: Making the game
+
+
+Current Status: game complete, planning on ai.
+
+Files:
+- /README.md
+- /game.py : game to be played by user, use python game.py to play
+- /game_ai.py : game with the controls in the hand of the Program itself 
 
 Dependencies:
 -python3
--- game.py
+++ game.py
@@ -1,4 +1,7 @@
 import pygame
+import numpy as np
+import math
+
 pygame.init()
 
 screen={
@@ -6,6 +9,10 @@ screen={
 	"height":500
 }
 screenHeight=500
+
+white = (255, 255, 255) 
+green = (0, 255, 0) 
+blue = (0, 0, 128) 
 
 win = pygame.display.set_mode((screen["width"],screen["height"]))
 
@@ -24,8 +31,13 @@ brick={
 	"width":60,
 	"height":25,
 	"color":(0,255,0),
-	"active": True
 }
+
+class Brick:
+	def __init__(self, x,y):
+		self.x=x
+		self.y=y
+		
 
 ball={
 	"x":screen["width"]/2,
@@ -38,28 +50,134 @@ ball={
 ball["x"]-=ball["width"]/2
 ball["y"]-=ball["height"]/2
 
+#Text
+font = pygame.font.Font('freesansbold.ttf', 32) 
+
+gameOverText = font.render('Game Over', True, green, blue) 
+gameOverTextRect = gameOverText.get_rect()  
+gameOverTextRect.center = (screen["width"] // 2, screen["height"] // 2 -100) 
+
+
+score=0
+time=0
+
+#3 lists of bricks
+list1=[]
+list2=[]
+list3=[]
+
+gapMax=15
+margin=6
+height1 = math.floor(1.2*margin+np.random.rand()*gapMax);
+height2 = height1 + math.floor(1.2*margin+np.random.rand()*gapMax)+brick["height"];
+height3 = height2 + math.floor(1.2*margin+np.random.rand()*gapMax)+brick["height"];
+
+x=math.floor(margin+np.random.rand()*gapMax)
+while x+brick["width"] < screen["width"]:
+	list1.append(Brick(x,height1))
+	x+=math.floor(margin+np.random.rand()*gapMax)
+	x+=brick["width"]
+
+x=math.floor(margin+np.random.rand()*gapMax)
+while x+brick["width"] < screen["width"]:
+	list2.append(Brick(x,height2))
+	x+=math.floor(margin+np.random.rand()*gapMax)
+	x+=brick["width"]
+	
+x=math.floor(margin+np.random.rand()*gapMax)
+while x+brick["width"] < screen["width"]:
+	list3.append(Brick(x,height3))
+	x+=math.floor(margin+np.random.rand()*gapMax)
+	x+=brick["width"]
+
 
 run=True
+gameOver=False
+pause=False
+won=False
+
+def getScore():
+	return math.floor(score*10-time/30)
 while run:
+	
+	if (not gameOver) and (not won): 
+		time=time+1;
 	pygame.time.delay(30)
+	
+		
+
 	
 	for event in pygame.event.get():
 		if event.type ==pygame.QUIT:
 			run=False
+		if event.type==pygame.KEYDOWN:
+			if event.key==pygame.K_p:
+				if pause:
+					pause=False
+				else:
+					pause=True	
+			if event.key==pygame.K_s:
+				list1=[]
+				list2=[]
+				list3=[]	
+
+	if pause:
+		continue	
+	
+	if gameOver:
+		win.fill((0,0,0))
+		win.blit(gameOverText,gameOverTextRect)
+		
+		scoreText = font.render('Score: '+str(getScore()), True, green, blue) 
+		scoreTextRect = scoreText.get_rect()  
+		scoreTextRect.center = (screen["width"] // 2, screen["height"] // 2+100) 
+
+		win.blit(scoreText,scoreTextRect)		
+
+		pygame.display.update()
+		continue
+	
+
+	if (not list1) and (not list2) and (not list3) :
+		won=True
+	if won:
+		win.fill((0,0,0))
+		scoreText = font.render('You Win, Score: '+str(getScore()), True, green, blue) 
+		scoreTextRect = scoreText.get_rect()  
+		scoreTextRect.center = (screen["width"] // 2, screen["height"] // 2) 
+
+		win.blit(scoreText,scoreTextRect)		
+		pygame.display.update()
+		continue
 	
 	keys=pygame.key.get_pressed()
-	
+
 	if keys[pygame.K_LEFT] and player["x"]>= player["vel"]:
 		player["x"]-=player["vel"]
 	if keys[pygame.K_RIGHT] and player["x"]<= screen["width"] - player["width"] - player["vel"]:
 		player["x"]+=player["vel"]
+	
+	#score Printing
+	text = font.render('Score: '+str(getScore()), True, green, blue) 
+	textRect = text.get_rect()  
+	textRect.center = (screen["width"] // 2, screen["height"] // 2) 
+	#win.fill((0,0,0),pygame.textRect.x)
+	win.blit(text,textRect)	
 
 	
-	#player handling
+	#bricks drawing
+	for var in list1:
+		pygame.draw.rect(win,brick["color"],(var.x,var.y,brick["width"],brick["height"]))
+	for var in list2:
+		pygame.draw.rect(win,brick["color"],(var.x,var.y,brick["width"],brick["height"]))
+	for var in list3:
+		pygame.draw.rect(win,brick["color"],(var.x,var.y,brick["width"],brick["height"]))
+	
+	#player drawing
 	win.fill((0,0,0),pygame.Rect(0,player["y"],screen["width"],player["height"]))
 	pygame.draw.rect(win,(0,0,255),(player["x"],player["y"],player["width"],player["height"]))
 	
-	#ball handling
+	#ball drawing
 	win.fill((0,0,0),pygame.Rect(ball["x"],ball["y"],ball["width"],ball["height"]))
 	ball["x"]+=ball["velX"]
 	ball["y"]+=ball["velY"]
@@ -74,7 +192,6 @@ while run:
 		ball["y"]-=ball["velY"]
 		ball["velY"]*=-1
 	elif ball["y"] >= screen["height"]-player["height"] -ball["velY"]:
-		#check coord of platform todo
 		if ball["x"] > player["x"]-ball["width"] and ball["x"]<player["x"]+player["width"]+ball["width"] :
 			ball["y"]-=ball["velY"]
 			ball["velY"]*=-1
@@ -83,11 +200,54 @@ while run:
 			centerPlayer=player["x"]+player["width"]/2
 			ball["velX"]=ball["velY"]*(centerPlayer-centerBall)/(max)
 	if ball["y"] >= screen["height"]:
-		#lost edit it todo
-		ball["y"]-=ball["velY"]
-		ball["velY"]*=-1
+		gameOver=True
 
 	pygame.draw.rect(win,(255,0,0),(ball["x"],ball["y"],ball["width"],ball["height"]))
 
 	pygame.display.update()
+
+	
+	#handle colision
+	col=False
+	if ball["y"] > height1-ball["height"] and ball["y"] < height1+brick["height"] :
+		for var in list1:
+			if ball["x"] > var.x-ball["width"] and ball["x"]<var.x+brick["width"] :
+				list1.remove(var)
+				#paint black too
+				win.fill((0,0,0),pygame.Rect(var.x ,var.y,brick["width"],brick["height"]))
+				col=True
+
+		if col:
+			#ball["velX"]*=-1
+			ball["velY"]*=-1
+			score=score+1
+			continue
+	if ball["y"] > height2-ball["height"] and ball["y"] < height2+brick["height"] :
+		for var in list2:
+			if ball["x"] > var.x-ball["width"] and ball["x"]<var.x+brick["width"] :
+				list2.remove(var)
+				win.fill((0,0,0),pygame.Rect(var.x ,var.y,brick["width"],brick["height"]))
+				col=True
+
+		if col:
+			#ball["velX"]*=-1
+			ball["velY"]*=-1
+			score=score+1
+			continue
+	
+	if ball["y"] > height3-ball["height"] and ball["y"] < height3+brick["height"] :
+		for var in list3:
+			if ball["x"] > var.x-ball["width"] and ball["x"]<var.x+brick["width"] :
+				list3.remove(var)
+				win.fill((0,0,0),pygame.Rect(var.x ,var.y,brick["width"],brick["height"]))
+				col=True
+
+		if col:
+			#ball["velX"]*=-1
+			ball["velY"]*=-1
+			score=score+1
+			continue
+	
+		
+			
 pygame.quit()
